5b71d2b9ede0919c5143b82d3c4a151a22b5e1fc
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2023-07-05 18:54:24 -0700

    Add github colorscheme comments.

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -2,6 +2,7 @@ local colorscheme = 'nightfox'
 
 -- monokai_pro
 -- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
+-- github_*
 
 local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not is_ok then
