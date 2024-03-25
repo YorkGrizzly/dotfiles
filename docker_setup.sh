0115183d15daaee17dbd784f8f0551d31227192c
Tien-Shuo Chang <50973240+tienshuoc@users.noreply.github.com>
2024-03-25 11:59:40 -0700

    Update docker_setup.sh: revert neovim

--- docker_setup.sh
+++ docker_setup.sh
@@ -3,7 +3,7 @@ if [[ "$1" == "ubuntu" ]]; then
 elif [[ "$1" == "rhel" ]]; then
     # Set metadata for packages expires after 2 days so that we don't update the DNF repository cache everytime (which is slow) we install a package.
     METADATA_EXPIRATION=$(( 60 * 60 * 24 * 7 ))
-    sudo dnf install -y --setopt metadata_expire=${METADATA_EXPIRATION} zsh zsh-syntax-highlighting tree ripgrep jq jsonnet ncdu neovim
+    sudo dnf install -y --setopt metadata_expire=${METADATA_EXPIRATION} zsh zsh-syntax-highlighting tree ripgrep jq jsonnet ncdu
     #fd-find hyperfine strace
 fi
 
